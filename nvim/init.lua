60b58c2159ab928b6f6238bf1321e09a73285e5d
shonenm <kotamatsu05223233@gmail.com>
2025-05-03 22:11:07 +0900

    imprv: modified cmp-error

--- nvim/init.lua
+++ nvim/init.lua
@@ -64,3 +64,14 @@ require("lazy").setup("plugins", {
     { "akinsho/bufferline.nvim", enabled = false },
   },
 })
+
+
+require("neo-tree").setup({
+  filesystem = {
+    filtered_items = {
+      visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+      hide_dotfiles = false,
+      hide_gitignored = true,
+    },
+  },
+})
