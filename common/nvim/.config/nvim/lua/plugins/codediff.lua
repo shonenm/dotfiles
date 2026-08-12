2e2ce2c522cde9f2d03f4399822cd0882f119c06
shonenm <131980874+shonenm@users.noreply.github.com>
2026-08-13 00:27:46 +0900

    feat(nvim): improve codereview workflow

--- common/nvim/.config/nvim/lua/plugins/codediff.lua
+++ common/nvim/.config/nvim/lua/plugins/codediff.lua
@@ -1,6 +1,15 @@
 return {
-  -- Disable snacks_picker's <leader>gd (Git Diff hunks) to free it for CodeDiff
-  { "folke/snacks.nvim", keys = { { "<leader>gd", false } } },
+  -- Disable snacks_picker's <leader>gd (Git Diff hunks) to free it for CodeDiff.
+  -- Reserve the first three rows for CodeDiff's fixed hunk indicator and wrap
+  -- long notifications instead of clipping them at the screen edge.
+  {
+    "folke/snacks.nvim",
+    keys = { { "<leader>gd", false } },
+    opts = {
+      notifier = { margin = { top = 3, right = 1, bottom = 0 } },
+      styles = { notification = { wo = { wrap = true } } },
+    },
+  },
   {
     "esmuellert/codediff.nvim",
     dependencies = { "MunifTanjim/nui.nvim" },
